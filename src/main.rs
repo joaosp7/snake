a9daec233c8b8ad79a4c439583c9b442f760dd3f
João Lucas Rodrigues <54967700+joaosp7@users.noreply.github.com>
2026-08-13 20:13:31 -0300

    chore/better lines

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use macroquad::prelude::*;
 
 #[macroquad::main("BasicShapes")]
+
 async fn main() {
+    let mut snake = vec![
+        (15, 15),
+        (14, 15),
+        (13, 15),
+    ];
     let cols = 30;
     let rows = 30;
     let cell_size = screen_width().min(screen_height()) / cols as f32 ;
@@ -17,33 +23,19 @@ async fn main() {
         // initial screen width -> 800
         clear_background(RED);
         //vertical lines
-        for i in 0..cols+1{
+        for i in 1..=cols{
             let x = origin_x + cell_size * i as f32;
             draw_line(x, origin_y, x, origin_y + grid_px_h, 2.0, BLUE);
         }
         //horizontal lines
-        for i in 1..rows+1{
+        for i in 1..=rows{
             let y = origin_y + cell_size * i as f32;
             draw_line(origin_x, y, origin_x + grid_px_w, y, 2.0, BLUE);
         }
+        draw_rectangle_lines(origin_x, origin_y, grid_px_w, grid_px_h, 2.0, BLUE);
         let fps = get_fps();
         println!("Current fps: {}",fps);
-       ;
-        println!("Screen width: {}", screen_width());
-        println!("Screen height : {}", screen_height());
-        
-        // vertical grid
-        //draw_line(delta_width, 0.0, delta_width, 600.00, 2.00, BLUE);
 
-       
-        //horizontal grid
-
-        //draw_line(0.0 , delta_height, 800.0, delta_height, 2.00, BLUE);
-              //draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        //draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        //draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-        //draw_ellipse(50.0, 50.0, 15.0, 15.0, 0.0, DARKBLUE);
-        //draw_text("HELLO", 20.0, 50.0, 20.0, DARKGRAY)
         
         next_frame().await
     }
